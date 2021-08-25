c7d361b00ab13d1d395711061fcdd235e4b8be86
Rohit Gandhi <rohit.r.gandhi@gmail.com>
2021-08-25 16:59:46 +0530

    Update

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
-const billAmount=document.querySelector("#billAmount");
+var billAmount = document.querySelector("#bill-amount");
 
-const cashPaid=document.querySelector("#cashPaid");
+var cashPaid = document.querySelector("#cash-paid");
+console.log(billAmount.value);
+console.log(cashPaid.value);
 
-const denominations=[2000,500,100,20,10,5,1]
-const notesgiven=document.querySelectorAll("#notesgiven");
+// const denominations = [2000, 500, 100, 20, 10, 5, 1]
+// const notesgiven = document.querySelectorAll("#notesgiven");
+// button.addEventListener("click", ()=>console.log("clicked"));
+// button.addEventListener("click", function onClick() {
+//             if(cashPaid > 0) {
+//                 if (cashPaid.value >= billAmount.value)
+//                  {
+//                     const returnAmount = cashPaid.value - billAmount.value;
 
-notesgiven.EventListener("click",function onClick(){
-if(cashPaid.value>=billAmount.value){
-    const returnAmount=cashPaid.value-billAmount.value;
-    function returnAmt();
-}
-else{
-    errorHandler();
-}
-})
+//                     // function returnAmt()
+//                 } 
+//                 else {
+//                     errorHandler();
+//                 }
+//             }
+//             else{
+//                     errorHandler();
+//             }})
 
-function returnAmt(){
-    for(var i=0; i <denominations.value; i++ )
-    {
-        returnAmount=returnAmount/denominations[i];
-        
-    }
-}
+//         function returnAmt() {
+//             for (var i = 0; i < denominations.value; i++) {
+//                 returnAmount = returnAmount / denominations[i];
+//                 returnAmount
 
-function errorHandler(){
-return console.log("Invalid input");
-}
+//             }
+//         }
+
+//         function errorHandler() {
+//             return console.log("Invalid input");
+//         }
